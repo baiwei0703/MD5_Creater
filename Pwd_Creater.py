79b286b8ebc0fca540fe252ddca3633629bfc712
baiwei0703 <34114426+baiwei0703@users.noreply.github.com>
2020-04-20 22:45:19 +0800

    Update Pwd_Creater.py

--- Pwd_Creater.py
+++ Pwd_Creater.py
@@ -2,35 +2,70 @@ import string
 import itertools
 import hashlib
 
-MIN_LENTH = input('Min Length is:')
-MAX_LENTH = input('Max Length is:')
-is_lower = input('Include lower letters? (y/n):').lower()
-is_upper = input('Include upper letters? (y/n):').lower()
-is_digit = input('Include numbers? (y/n):').lower()
-task_str = ''
-file_name = 'combina_{}-{}-lud.txt'.format(MIN_LENTH, MAX_LENTH)
-if is_lower == 'y':
-    task_str = task_str + string.ascii_lowercase
-else:
-    file_name = file_name.replace('l', '_')
-if is_upper == 'y':
-    task_str = task_str + string.ascii_uppercase
-else:
-    file_name = file_name.replace('u', '_')
-if is_digit == 'y':
-    task_str = task_str + string.digits
-else:
-    file_name = file_name.replace('d', '_')
-print('I will compute %s times!', format(10 ** len(task_str), ','))
 
-proceedOn = input('Confirm proceed? (yes/no):')
+def check_input(info):
+    if not info['MAX_LENTH'].isdigit():
+        return 'MAX_LENTH is NOT a number!'
+    elif not info['MIN_LENTH'].isdigit():
+        return 'MIN_LENTH is NOT a number!'
+    elif int(info['MIN_LENTH']) > int(info['MAX_LENTH']):
+        return 'The MIN_LENTH is GREATER than the MAX_LENTH!'
+    elif not (info['is_lower'] or info['is_upper'] or info['is_digit']):
+        return 'Did NOT choose any string to combine!'
+    else:
+        return False
+
+
+def f_name(info):
+    task_str = ''
+    file_name = 'combine_{}-{}-lud.txt'.format(info['MIN_LENTH'], info['MAX_LENTH'])
+    if info['is_lower'].lower() == 'y':
+        task_str = task_str + string.ascii_lowercase
+    else:
+        file_name = file_name.replace('l', '_')
+    if info['is_upper'].lower() == 'y':
+        task_str = task_str + string.ascii_uppercase
+    else:
+        file_name = file_name.replace('u', '_')
+    if info['is_digit'].lower() == 'y':
+        task_str = task_str + string.digits
+    else:
+        file_name = file_name.replace('d', '_')
+    times = 0
+    for length in range(int(info['MIN_LENTH']), int(info['MAX_LENTH']) + 1):
+        times = times + len(task_str) ** length
+    print('I will compute %s times!' % format(times, ','))
+
+    proceed_on = input('Confirm proceed? (yes/no):')
+    return proceed_on, file_name, task_str
+
+
+while 1:
+    MIN_LENTH = input('Min Length is:').strip()
+    MAX_LENTH = input('Max Length is:').strip()
+    is_lower = input('Include lower letters? (y/n):').strip()
+    is_upper = input('Include upper letters? (y/n):').strip()
+    is_digit = input('Include numbers? (y/n):').strip()
+    input_info = {
+        'MIN_LENTH': MIN_LENTH,
+        'MAX_LENTH': MAX_LENTH,
+        'is_lower': is_lower,
+        'is_upper': is_upper,
+        'is_digit': is_digit
+    }
+    res = check_input(input_info)
+    if res:
+        print(res)
+    else:
+        proceedOn, fileName, taskStr = f_name(input_info)
+        break
 
 if proceedOn.lower() == 'yes':
-    with open(file_name, 'w') as f:
-        print('Initializer Done, Start Writing!\n The file name is %s', file_name)
+    with open(fileName, 'w') as f:
+        print('Initialization has Done, Start Writing!\n The file name is %s', fileName)
         for t in range(int(MIN_LENTH), int(MAX_LENTH) + 1):
             print('Current Mission:', t)
-            for i in itertools.combinations_with_replacement(task_str, t):
+            for i in itertools.combinations_with_replacement(taskStr, t):
                 i = ''.join(i)
                 i_md5 = hashlib.md5(i.encode(encoding='utf-8')).hexdigest()
                 f.write(i + '\t' + i_md5 + '\n')
